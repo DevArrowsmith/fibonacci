42438c6d889bc8dcca3ba6c08b20ca28d58f1da1
GummiCode <gummicodeblog@gmail.com>
2021-03-07 15:21:56 +0000

    Modify renderFibonacci to empty the numeral container prior to rendering the new sequence.

--- js/renderFibonacci.js
+++ js/renderFibonacci.js
@@ -2,6 +2,8 @@ const renderFibonacci = (sequence, container) => {
 
   const fibonacciContainer = document.getElementById(`${container}`);
 
+  fibonacciContainer.innerHTML = '';
+
   sequence.forEach(number => {
     fibonacciContainer.insertAdjacentHTML('beforeend',`<p>${number}</p>`);
   })
